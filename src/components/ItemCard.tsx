1b2ac95b3a826cd5ab41ce79406f8b4a050bc4bc
tillyninjaspace <squealingpet@gmail.com>
2021-11-02 18:35:13 -0700

    single item functional

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,53 +1,54 @@
-import React, {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import React from "react";
+import {useParams, Link} from "react-router-dom";
+import './style.css';
 
-
-const itemURL = 'http://localhost:3001/items/'
+const itemURL = 'http://localhost:3001/items/';
 
 interface Item {
   id: string;
   name: string;
   description: string;
   imageUrls: [];
-  // variants: [];
   url: string;
-
-}
-
+   variants: [];
+};
 
 interface ChildProps {
   posts: Item[]
-}
+};
 
 export const ItemCard: React.FC<ChildProps>= (props) => {
-
-  // const itemId = useParams()
   const { itemId } = useParams<{ itemId: string }>();
   console.log("Item ID:", itemId)
 
   const foundItem = props.posts.find(singleItem => itemId === singleItem.id);
   console.log("Find Item", foundItem)
 
-
   return (
-    <div>
+    <div className="itemMain">
+      
       {foundItem &&
       <>
+        <section>
+        <p><Link to={"/"}>Back to Marketplace</Link></p>
+        {
+          foundItem.imageUrls.map((picture:any, idx ) =>
+            <img key={idx} src={picture.url}/>
+          )
+        }
+        </section>
+        <section>
         <h2>{foundItem.name}</h2>
         <p>{foundItem.id}</p>
         <p>{foundItem.description}</p>
         {
-          foundItem.imageUrls.map((picture:any, idx ) =>
-            <p key={idx}>{picture.url}</p>
+          foundItem.variants.map((variant:any, idx ) =>
+            <p key={idx}>{variant.name}</p>
           )
         }
-        
+        </section>
       </>  
       } 
-
-
     </div>
   )
-  
-  
 };
--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -2,7 +2,7 @@
 import React, {useEffect, useState} from "react";
 import {BrowserRouter as Router,
         Route,
-        Link } from 'react-router-dom';
+        Link } from "react-router-dom";
 import { ItemCard } from "components";
 
 const URL = 'http://localhost:3001/items';
@@ -12,56 +12,45 @@ export const ItemList: React.FC = () => {
 
   const fetchPosts = async () => {
     try {
-        console.log("starting to fetch")
         const data = await fetch(URL)
-        console.log(data)
         const response = await data.json();
         console.log("Response", response)
         return response
     } catch (error) {
         console.error(error)
-    } finally {
-        //Maybe add interstitial
-    }
+    } 
 };
 
 useEffect( () =>{
   fetchPosts()
   .then(data => {
       setPosts(data)
-      console.log("Posts", posts)
   })
   .catch(error => {
       console.error(error)
   })
 },[]);
 
-// var fetchedPostList: arrayOfObjects[] = posts;
-
   return  <Router>
-
 
   <div style={{display:"flex", flexWrap: "wrap",
   justifyContent: "center"}}>
-  {/* <h1>Build Stuff Here for Postal</h1> */}
- 
 
-<Route exact path = "/">
+  <Route exact path = "/">
   {posts && posts.map((post) => 
     <div key={post.id} style={{border: ".5em solid gray",
       maxWidth: "300px", padding: ".5em", display: "flex",
       flexDirection: "column", margin: ".25em", justifyContent: "space-between"
       }}>
-      <p>{post.id}</p>
       <h3>{post.name}</h3>
-      <p>{post.description}</p>
+      <p>Description: {post.description}</p>
 
       { post && post.variants.length > 0 && 
       post.variants.length < 5
        ?
         post.variants.map((variant:any) =>
         <p key={variant.id}>
-          Items: {variant.name}
+          Item: {variant.name}
         </p>
         )
         : post.variants.length > 5 &&
@@ -79,7 +68,7 @@ useEffect( () =>{
 
     </div>
     )}
-</Route>  
+  </Route>  
 
 
   <Route exact path="/:itemId">
